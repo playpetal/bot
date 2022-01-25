a8654a29e208e453a5f9891e262f4f5b5cf7d36e
dreamingkills <realrtfl@gmail.com>
2022-01-24 22:14:07 -0500

    add image preview to prefab creation

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,7 +1,8 @@
+import axios from "axios";
 import { Message, TextChannel } from "eris";
 import { bot } from "..";
 import { prefabCreationManager } from "../lib/mod/createCard";
-import { Embed } from "../struct/embed";
+import { Embed, ErrorEmbed } from "../struct/embed";
 import { Event } from "../struct/event";
 
 const run = async function (msg: Message) {
@@ -16,11 +17,29 @@ const run = async function (msg: Message) {
 
   prefabCreationManager.setImage(msg.author, attachment.url);
 
-  const embed = new Embed().setDescription(
-    "**Prefab Review**\nPlease review the prefab information above, and the attached image.\nIf it all looks correct, press the button below.\n\nOtherwise, you may send another message or wait for the timer to expire."
-  );
+  const card = (await axios.get(`${process.env.ONI_URL}/card`, {
+    data: {
+      image: attachment.url,
+      input: "#FFAACC",
+      name: "Prefab",
+      id: Date.now() + instance.characterId * Math.round(Math.random() * 1000),
+    },
+  })) as { data: { image: string | null; error: string | null } };
 
   const channel = (await bot.getRESTChannel(msg.channel.id)) as TextChannel;
+
+  if (!card.data.image)
+    return await channel.createMessage({
+      embeds: [
+        new ErrorEmbed("An error occurred while trying to generate a preview."),
+      ],
+    });
+
+  const embed = new Embed()
+    .setDescription(
+      `**Prefab Review**\nPlease review the prefab information above, and the attached image [[direct link]](${card.data.image}).\nIf it all looks correct, press the button below.\n\nOtherwise, you may send another message or wait for the timer to expire.`
+    )
+    .setImage(card.data.image);
 
   await channel.createMessage({
     embeds: [embed],
