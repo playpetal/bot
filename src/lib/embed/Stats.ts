9ea78fae9d3e5ca41bfb9750d8d615fb2c7c62b1
dreamingkills <realrtfl@gmail.com>
2022-02-03 10:56:48 -0500

    (fix) improve readability + fix NaN bug in stats embed

--- src/lib/embed/Stats.ts
+++ src/lib/embed/Stats.ts
@@ -1,33 +1,43 @@
 import { Account } from "petal";
 import { Embed } from "../../struct/embed";
 import { displayName } from "../util/displayName";
+import { emoji } from "../util/formatting/emoji";
+import { strong } from "../util/formatting/strong";
 
 export function getStatsEmbed(account: Account) {
+  const {
+    rollCount,
+    gtsTotalGames,
+    gtsGuessCount,
+    gtsTotalRewards,
+    gtsTotalTime,
+  } = account.stats;
+
+  const totalGuessTime = (gtsTotalTime / 1000).toFixed(2);
+  const _avgGuessTime = gtsTotalTime / gtsTotalGames / 1000;
+  const avgGuessTime = isNaN(_avgGuessTime)
+    ? `0.00s`
+    : `${_avgGuessTime.toFixed(2)}s`;
+
   const embed = new Embed()
     .setDescription(
-      `<:user:930918872473796648> ${displayName(account)}` +
+      `${emoji.user} ${displayName(account)}` +
         `\nregistered <t:${Math.floor(account.createdAt / 1000)}:R>\n\u200b`
     )
     .addField({
       name: "gameplay",
       value:
-        `<:dice:938013692593860639> rolled **${account.stats.rollCount.toLocaleString()}** times` +
-        `\n<:song:930932998138900540> guessed **${account.stats.gtsTotalGames.toLocaleString()}** songs`,
+        `${emoji.dice} rolled ${strong(rollCount)} times` +
+        `\n${emoji.song} guessed ${strong(gtsTotalGames)} songs`,
       inline: true,
     })
     .addField({
       name: "songs",
       value:
-        `<:song:930932998138900540> **${account.stats.gtsGuessCount.toLocaleString()}** total guesses` +
-        `\n<:song:930932998138900540> **${(
-          account.stats.gtsTotalTime / 1000
-        ).toFixed(2)}**s spent guessing` +
-        `\n<:song:930932998138900540> average guess time: **${(
-          account.stats.gtsTotalTime /
-          account.stats.gtsTotalGames /
-          1000
-        ).toFixed(2)}s**` +
-        `\n<:song:930932998138900540> earned <:petals:930918815225741383> **${account.stats.gtsTotalRewards.toLocaleString()}**`,
+        `${emoji.song} ${strong(gtsGuessCount)} total guesses` +
+        `\n${emoji.song} **${totalGuessTime}**s spent guessing` +
+        `\n${emoji.song} average guess time: ${strong(avgGuessTime)}` +
+        `\n${emoji.song} earned ${emoji.petals} ${strong(gtsTotalRewards)}`,
       inline: true,
     })
     .setThumbnail("https://cdn.playpetal.com/avatars/default.png")
