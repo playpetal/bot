6c394479fdecaad9dae1d48f84e4ad1cb95b3768
dreamingkills <realrtfl@gmail.com>
2022-02-03 10:28:05 -0500

    (fix) add missing emojis

--- src/lib/util/formatting/emoji.ts
+++ src/lib/util/formatting/emoji.ts
@@ -1,4 +1,9 @@
 export const emoji = {
   user: "<:user:930918872473796648>",
   title: "<:title:930918843537309776>",
+  seed: "<:seed:937994849553113088>",
+  sprout: "<:sprout:937994864606457856>",
+  bud: "<:bud:937994864866492416>",
+  flower: "<:flower:937994864837136414>",
+  bloom: "<:petal:917578760449060995>",
 } as const;
