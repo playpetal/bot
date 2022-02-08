5a6bbd071e160a8661e66db96031bbcd9b8d003c
dreamingkills <realrtfl@gmail.com>
2022-02-07 19:28:59 -0500

    (feature) add cancel-gts component

---
+++ src/components/cancel-gts.ts
@@ -0,0 +1,35 @@
+import { ComponentInteraction } from "eris";
+import { PartialUser } from "petal";
+import { getUser } from "../lib/graphql/query/GET_USER";
+import { redis } from "../lib/redis";
+import { Component } from "../struct/component";
+import { Embed } from "../struct/embed";
+import { BotError } from "../struct/error";
+
+async function run(interaction: ComponentInteraction, user: PartialUser) {
+  const [_customId, accountIdStr] = interaction.data.custom_id.split("?");
+  if (!accountIdStr) return;
+
+  const accountId = parseInt(accountIdStr);
+  const account = await getUser({ id: accountId });
+
+  if (!account || user.id !== account.id)
+    throw new BotError("that's not your game!");
+
+  const gameStr = await redis.get(`gts:game:${accountId}`);
+
+  if (!gameStr) throw new BotError("this game doesn't exist!");
+
+  await redis.del(`gts:game:${accountId}`);
+
+  const embed = new Embed()
+    .setColor("#F04747")
+    .setDescription("**Better luck next time!**\nYou cancelled this game.");
+  await interaction.editOriginalMessage({ embeds: [embed], components: [] });
+
+  return;
+}
+
+const command = new Component("cancel-gts", run);
+
+export default command;
