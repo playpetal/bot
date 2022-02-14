aeb733175f3cd3783467f7cab4ec773fc38ded41
dreamingkills <realrtfl@gmail.com>
2022-02-14 17:27:14 -0500

    (refactor) add components builder

---
+++ src/lib/util/component/index.ts
@@ -0,0 +1,33 @@
+const buttonStyles = {
+  blue: 1,
+  gray: 2,
+  green: 3,
+  red: 4,
+} as const;
+
+type Button = {
+  type: 2;
+  label: string;
+  custom_id: string;
+  style: 1 | 2 | 3 | 4;
+};
+
+export function button(
+  label: string,
+  customId: string,
+  style: keyof typeof buttonStyles
+) {
+  return {
+    type: 2 as const,
+    label,
+    custom_id: customId,
+    style: buttonStyles[style],
+  };
+}
+
+export function row(...buttons: Button[]) {
+  return {
+    type: 1 as const,
+    components: buttons,
+  };
+}
