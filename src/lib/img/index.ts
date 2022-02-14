a9cd05e96a657d8ea8b35e62178cc5dce95632fa
dreamingkills <realrtfl@gmail.com>
2022-02-14 18:17:13 -0500

    (fix) fix frame hashes

--- src/lib/img/index.ts
+++ src/lib/img/index.ts
@@ -3,18 +3,13 @@ import { Card } from "petal";
 import { logger } from "../logger";
 
 export async function getCardImage(card: Card): Promise<Buffer> {
-  const {
-    data: { hash },
-  } = (await axios.get(`${process.env.ONI_URL!}/hash`, {
-    headers: { Authorization: process.env.ONI_SHARED_SECRET! },
-    data: { id: card.prefab.id },
-  })) as { data: { hash: string } };
+  const hash = await getHash(card.prefab.id);
 
   try {
     const { data } = (await axios.post(`${process.env.ONI_URL!}/card`, [
       {
         frame: card.hasFrame
-          ? `https://cdn.playpetal.com/f/${hash}.png`
+          ? `https://cdn.playpetal.com/f/${await getHash(card.id)}.png`
           : `#${card.tint.toString(16)}`,
         name: card.prefab.character.name,
         id: card.id,
@@ -29,4 +24,15 @@ export async function getCardImage(card: Card): Promise<Buffer> {
     logger.error(e);
     return Buffer.alloc(0);
   }
+}
+
+export async function getHash(number: number): Promise<string> {
+  const {
+    data: { hash },
+  } = (await axios.get(`${process.env.ONI_URL!}/hash`, {
+    headers: { Authorization: process.env.ONI_SHARED_SECRET! },
+    data: { id: number },
+  })) as { data: { hash: string } };
+
+  return hash;
 }
