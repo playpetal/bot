ad2d6e887b76a617e90f0ac1562af061421d1440
dreamingkills <realrtfl@gmail.com>
2022-03-03 18:38:34 -0500

    (fix) fix minigame logging

--- src/lib/logger/minigame/index.ts
+++ src/lib/logger/minigame/index.ts
@@ -2,5 +2,15 @@ import { UnknownMinigame } from "petal";
 import { logger } from "..";
 
 export function logMinigame(minigame: UnknownMinigame): void {
-  logger.info(minigame);
+  logger.info({
+    message: {
+      ...minigame,
+      guild: undefined,
+      channel: undefined,
+      message: undefined,
+      guildId: minigame.guild,
+      channelId: minigame.channel,
+      messageId: minigame.message,
+    },
+  });
 }
