b3a968ba0ecba636e4c9e1addb462555172ec1d5
dreamingkills <realrtfl@gmail.com>
2022-03-18 00:20:49 -0400

    (tests) add formatting test

---
+++ tests/lib/formatting.test.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { displayName } from "../../src/lib/util/displayName";
+import { strong } from "../../src/lib/util/formatting/strong";
+
+describe("lib", () => {
+  describe("formatting", () => {
+    it("should correctly render display names", () => {
+      const name = displayName({
+        username: "Test",
+        title: { title: "%u the Tester" },
+        id: 0,
+        discordId: "TEST",
+      });
+
+      expect(name).to.equal("**Test** the Tester");
+    });
+
+    it("should correctly strong-ify text and numbers", () => {
+      expect(strong("Test!")).to.equal("**Test!**");
+      expect(strong(7270)).to.equal("**7,270**");
+    });
+  });
+});
