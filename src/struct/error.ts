7b5d2534706fef29f50cab5ddba32ad7ec44fb04
dreamingkills <realrtfl@gmail.com>
2022-04-08 22:09:40 -0400

    (refactor) add unexpected error class

--- src/struct/error.ts
+++ src/struct/error.ts
@@ -2,9 +2,17 @@ import { Components } from "petal";
 
 export class BotError extends Error {
   components: Components = [];
+  name = "BotError";
   constructor(message: string, components?: Components) {
     super(message);
-    this.name = "BotError";
     this.components = components || [];
+  }
+}
+
+export class UnexpectedError extends BotError {
+  constructor() {
+    super(
+      "**an unexpected error occurred.**\nplease try again in a few moments."
+    );
   }
 }
